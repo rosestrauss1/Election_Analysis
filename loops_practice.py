277b2ae743523ebe792d3516d37a37a16d746153
Rose Strauss <rosestrauss@Roses-MacBook-Pro-2.local>
2021-11-09 20:51:07 -0800

    Adding the election audit code.

---
+++ loops_practice.py
@@ -0,0 +1,17 @@
+'loop types: while loops and for loops'
+
+'while loop:'
+'We set the variable x = 0 before we enter the loop.'
+x = 0
+'We test if x is less than or equal to 5.'
+while x <= 5:
+    print(x)
+    x = x + 1
+
+'^If this condition is true, then we print the value of x. With x = x + 1, we increment x by 1.'
+
+count = 7
+while count < 1:
+    print("Hello World")
+
+'for loop:'
